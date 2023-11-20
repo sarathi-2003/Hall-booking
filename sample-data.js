82c07a6dd715c3cc02947252ec95f182a9f64132
sarathi-2003 <sarathip846@gmail.com>
2023-11-20 14:03:05 +0530

    Complete the All Api

---
+++ sample-data.js
@@ -0,0 +1,35 @@
+let rooms = [{
+    roomId:"R1",
+    seatsAvailable:"4",
+    amenities:"tv,ac,heater",
+    pricePerhr:"100"
+}];
+
+let bookings =[{
+    customer:"Selva",
+    bookingDate:"2023-18-11",
+    startTime:"12:00pm",
+    endTime:"11:59am",
+    bookingID:"B1",
+    roomId:"R1",
+    status:"booked",
+    booked_On:"1-12-2023"
+}];
+
+let customers = [
+    {
+        name :"Selva",
+        bookings:[
+            {
+                customer:"Selva",
+                bookingDate:"2023-18-11",
+                startTime:"12:00pm",
+                endTime:"11:59am",
+                bookingID:"B1",
+                roomId:"R1",
+                status:"booked",
+                booked_On:"1-12-2023"
+            }
+        ]
+    }
+];
